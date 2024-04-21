7b348d4d901a3fcf656a439b704f86fed29a4ef1
Kevin Uehara <uehara.kevin@gmail.com>
2024-04-21 12:29:09 -0300

    Testing CI

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome Kevin
+          Welcome Testing
         </a>
       </header>
     </div>
